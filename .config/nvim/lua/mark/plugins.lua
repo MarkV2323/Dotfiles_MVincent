392eede197b94303b7212fe4f6e02be3f2adf20a
Mark Vincent <41304553+MarkV2323@users.noreply.github.com>
2024-06-13 00:09:23 -0600

    Added LSP-Zero plugin to my NVim workflow

--- .config/nvim/lua/mark/plugins.lua
+++ .config/nvim/lua/mark/plugins.lua
@@ -25,4 +25,20 @@ return require('packer').startup(function(use)
     end,
     }
 
+  -- LSP-Zero allows for an all in one LSP, Snippets and Code Completion
+  -- Ensure you have NodeJS installed in order to install LSPs through Mason
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment the two plugins below if you want to manage the language servers from neovim
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {'neovim/nvim-lspconfig'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
+  }
+
 end)
