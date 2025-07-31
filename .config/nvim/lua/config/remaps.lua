41d53f27e72a7c2d4e8a9f640c8f4f4dea7b6220
Mark Vincent <41304553+MarkV2323@users.noreply.github.com>
2025-07-30 22:26:37 -0500

    Updated remaps with cleanup and new maps

--- .config/nvim/lua/config/remaps.lua
+++ .config/nvim/lua/config/remaps.lua
@@ -1,39 +1,88 @@
 -- Common Remaps
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 -- Source init.vim
-vim.keymap.set("n", "<leader>so", "<cmd>so ~/.config/nvim/init.lua<CR>")
+keymap.set("n", "<leader>so", "<cmd>so ~/.config/nvim/init.lua<CR>", opts)
 
 -- NetRw
-vim.keymap.set("n", "<leader>rw", vim.cmd.Ex)
+keymap.set("n", "<leader>rw", vim.cmd.Ex)
 
 -- Create sesh
-vim.keymap.set("n", "<leader>ss", "<cmd>mksession!<CR>")
+keymap.set("n", "<leader>ss", "<cmd>mksession!<CR>", opts)
 
 -- Save & Exit quickly
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
-vim.keymap.set("n", "<leader>sq", "<cmd>wq<CR>")
+keymap.set("n", "<leader>w", "<cmd>w<CR>", opts)
+keymap.set("n", "<leader>q", "<cmd>q<CR>", opts)
+keymap.set("n", "<leader>sq", "<cmd>wq<CR>", opts)
 
 -- Exit search
-vim.keymap.set("n", "<leader>h", "<cmd>noh<CR>")
+keymap.set("n", "<leader>h", "<cmd>noh<CR>", opts)
 
 -- Tabs
-vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>")
-vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>")
-vim.keymap.set("n", "<leader>1", "1gt")
-vim.keymap.set("n", "<leader>2", "2gt")
-vim.keymap.set("n", "<leader>3", "3gt")
-vim.keymap.set("n", "<leader>4", "4gt")
+keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", opts)
+keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", opts)
+keymap.set("n", "<leader>1", "1gt", opts)
+keymap.set("n", "<leader>2", "2gt", opts)
+keymap.set("n", "<leader>3", "3gt", opts)
+keymap.set("n", "<leader>4", "4gt", opts)
 
 -- Buffer
-vim.keymap.set("n", "<leader>bs", "<cmd>sp<CR>")
-vim.keymap.set("n", "<leader>bv", "<cmd>vs<CR>")
-vim.keymap.set("n", "<leader><left>", "<c-w><left>")
-vim.keymap.set("n", "<leader><right>", "<c-w><right>")
-vim.keymap.set("n", "<leader><up>", "<c-w><up>")
-vim.keymap.set("n", "<leader><down>", "<c-w><down>")
+keymap.set("n", "<leader>bs", "<cmd>sp<CR>", opts)
+keymap.set("n", "<leader>bv", "<cmd>vs<CR>", opts)
+keymap.set("n", "<leader><left>", "<c-w><left>", opts)
+keymap.set("n", "<leader><right>", "<c-w><right>", opts)
+keymap.set("n", "<leader><up>", "<c-w><up>", opts)
+keymap.set("n", "<leader><down>", "<c-w><down>", opts)
+
+-- Resize windows
+keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-- Better visual mode movement
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
 
 -- Quicker scrollin
-vim.keymap.set("n", "<c-y>", "3<c-y>")
-vim.keymap.set("n", "<c-e>", "3<c-e>")
+keymap.set("n", "<c-y>", "3<c-y>", opts)
+keymap.set("n", "<c-e>", "3<c-e>", opts)
 
+-- Join lines without moving cursor
+keymap.set('n', 'J', 'mzJ`z', opts)
+
+-- Keep cursor centered when searching
+keymap.set('n', 'n', 'nzzzv', opts)
+keymap.set('n', 'N', 'Nzzzv', opts)
+
+-- Paste without overwriting clipboard
+keymap.set('x', '<leader>p', '"_dP', opts)
+
+-- Yank to system clipboard
+keymap.set({ 'n', 'v' }, '<leader>y', '"+y', opts)
+keymap.set('n', '<leader>Y', '"+Y', opts)
+
+function ToggleRelativeNumbers()
+  if vim.wo.relativenumber then
+    vim.wo.relativenumber = false
+  else
+    vim.wo.relativenumber = true
+    vim.wo.number = true  -- ensure number is on when using relative
+  end
+end
+vim.keymap.set('n', '<leader>n', ToggleRelativeNumbers, { desc = 'Toggle relative line numbers' })
+
+function ToggleWrap()
+  vim.wo.wrap = not vim.wo.wrap
+  -- Optional: improve experience when wrap is on
+  if vim.wo.wrap then
+    vim.wo.linebreak = true       -- wrap at word boundaries
+    vim.opt.showbreak = '↪ '      -- show nice wrap symbol
+    print("Wrap: ON")
+  else
+    vim.wo.linebreak = false
+    print("Wrap: OFF")
+  end
+end
+vim.keymap.set('n', '<leader>z', ToggleWrap, { desc = 'Toggle word wrap' })
